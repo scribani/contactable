fd41704665f63891a3df6687ca194f0f46a011fa
Scribani <scribani.andres@gmail.com>
2021-09-24 06:25:03 -0500

    Add contact-details component

--- scripts/store.js
+++ scripts/store.js
@@ -9,11 +9,27 @@ async function setInitialData() {
 function clear() {
   this.contacts = [];
   this.favorites = [];
+  this.currentSection = "";
+  this.currentContactId = null;
+}
+
+function getCurrentContact() {
+  return this.contacts.find((contact) => contact.id === this.currentContactId);
+}
+
+function deleteContact(id) {
+  console.log(this.contacts);
+  this.contacts = this.contacts.filter((contact) => contact.id !== id);
+  console.log(this.contacts);
 }
 
 const STORE = {
   contacts: [],
   favorites: [],
+  currentSection: "",
+  currentContactId: null,
+  getCurrentContact,
+  deleteContact,
   setInitialData,
   clear,
 };
