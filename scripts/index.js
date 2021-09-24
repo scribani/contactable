b47b0323ad060a61b40882a0dd21b067b75968c2
ever <ever864@gmail.com>
2021-09-24 01:26:48 -0500

    add new changes for login in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,25 @@
+import Login from "./pages/login.js";
 import { login } from "./services/sessions_fetcher.js";
 import STORE from "./store.js";
 
 (async function () {
-  const userData = await login("scribani@test.com", "123456");
-  sessionStorage.setItem("token", userData.token);
+  
+  if(sessionStorage.getItem("token")){
+    try{
+    const userData = await login("scribani@test.com", "123456");
+    sessionStorage.setItem("token", userData.token);
+    await STORE.setInitialData();
+    console.log(STORE.contacts, STORE.favorites);
+    }  catch (e){
+      console.log(e);
+      alert(e);
+      sessionStorage.removeItem("token");
+      DOMHandler.render(Login);
+    }
+  }else {
+    DOMHandler.render(Login);
 
-  await STORE.setInitialData();
-  console.log(STORE.contacts, STORE.favorites);
+  }
+  
+  
 })();
