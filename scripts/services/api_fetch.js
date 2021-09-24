6c469555f5900c464805231cef6d68b35eaf6041
eder <edra1791@gmail.com>
2021-09-23 22:17:11 -0500

    function login and logout

---
+++ scripts/services/api_fetch.js
@@ -0,0 +1,15 @@
+import { BASE_URL } from "../constants.js";
+
+export const apiFetch = async (endpoint, method = "GET", headers, body) => {
+  const response = await fetch(`${BASE_URL}${endpoint}`, {
+    method,
+    headers,
+    body: JSON.stringify(body),
+  });
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.errors);
+  }
+  if (response.status === 204) return {};
+  return await response.json();
+};
---
+++ scripts/services/sessions_fetcher.js
@@ -0,0 +1,16 @@
+import { apiFetch } from "./api_fetch.js";
+
+export const login = (email, password) =>
+  apiFetch(
+    "login",
+    "POST",
+    {
+      "Content-Type": "application/json",
+    },
+    { email, password }
+  );
+
+export const logout = () =>
+  apiFetch("logout", "DELETE", {
+    Authorization: `Token token=${sessionStorage.getItem("token")}`,
+  });
