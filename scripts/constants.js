83b38ed05e923db1552539423b0f33f7fa6d21c3
Scribani <scribani.andres@gmail.com>
2021-09-24 06:23:42 -0500

    Add script for main page

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,1 +1,4 @@
 export const BASE_URL = `https://contactable-api.herokuapp.com/`;
+export const CREATE_CONTACT = "create";
+export const CONTACT_DETAILS = "show";
+export const EDIT_CONTACT = "edit";
