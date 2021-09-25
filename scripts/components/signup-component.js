da5e774285a45b7c8d9b1510dc788afca598acd4
Scribani <scribani.andres@gmail.com>
2021-09-25 14:25:20 -0500

    Refactorization to Login page

--- scripts/components/signup-component.js
+++ scripts/components/signup-component.js
@@ -1,41 +1,13 @@
-import { LOGIN } from "../constants.js";
-import DOMHandler from "../dom_handler.js";
-import Login from "../pages/login.js";
-import Main from "../pages/main.js";
-import { signup } from "../services/signup_fetcher.js";
-import STORE from "../store.js";
-
-async function signupUser(e) {
-  e.preventDefault();
-  const { email, password } = e.target;
-  const userData = await signup(email.value, password.value);
-  sessionStorage.setItem("token", userData.token);
-  await STORE.setInitialData();
-  DOMHandler.render(Main);
-}
-
-function onLogin(e) {
-  e.preventDefault();
-  STORE.currentSection = LOGIN;
-  DOMHandler.render(Login);
-}
-
 const signupComponent = () => {
   return {
     title: "Signup",
     footer: `
-    <a class="js-login button-blue mp-r-25" href="#login">Login</a>
+    <a class="js-link button-blue mp-r-25" href="#login">Login</a>
     <button class="button-blue button-login mp-r-16">
       Create Account
     </button>
     `,
-    addEventListeners: function () {
-      const form = document.querySelector(".js-login-form");
-      const loginBtn = document.querySelector(".js-login");
-
-      form.addEventListener("submit", signupUser);
-      loginBtn.addEventListener("click", onLogin);
-    },
+    addEventListeners: function () {},
   };
 };
 
--- scripts/pages/login.js
+++ scripts/pages/login.js
@@ -1,13 +1,44 @@
 import loginComponent from "../components/login-component.js";
 import signupComponent from "../components/signup-component.js";
+import { LOGIN, SIGNUP } from "../constants.js";
+import DOMHandler from "../dom_handler.js";
+import { login } from "../services/sessions_fetcher.js";
+import { signup } from "../services/signup_fetcher.js";
 import STORE from "../store.js";
+import Main from "./main.js";
 
-function getSection(currentSection) {
-  return currentSection === "login" ? loginComponent() : signupComponent();
+async function onFormSubmit(e) {
+  e.preventDefault();
+  const section = STORE.currentSection;
+
+  try {
+    const { email, password } = e.target;
+    const userData = await (section === "login"
+      ? login(email.value, password.value)
+      : signup(email.value, password.value));
+    sessionStorage.setItem("token", userData.token);
+    await STORE.setInitialData();
+    DOMHandler.render(Main);
+  } catch (e) {
+    console.log(e);
+    alert(e);
+  }
+}
+
+function onFollowLink(e) {
+  e.preventDefault();
+  const section = STORE.currentSection;
+
+  STORE.currentSection = section === "login" ? SIGNUP : LOGIN;
+  DOMHandler.render(Login);
 }
 
 const Login = (function () {
   let section;
+
+  function getSection(currentSection) {
+    return currentSection === "login" ? loginComponent() : signupComponent();
+  }
 
   return {
     render: function () {
@@ -21,7 +52,7 @@ const Login = (function () {
           <form class="js-login-form">
             <div class="login_inputs">
               <div>
-                <input class="input-login" type="email" name="email" placeholder="email" />
+                <input class="input-login" type="email" name="email" placeholder="email" autofocus />
               </div>
               <div>
                 <input class="input-login" type="password" name="password" placeholder="password" />
@@ -36,7 +67,11 @@ const Login = (function () {
       `;
     },
     addEventListeners: function () {
-      section.addEventListeners();
+      const form = document.querySelector(".js-login-form");
+      const link = document.querySelector(".js-link");
+
+      form.addEventListener("submit", onFormSubmit);
+      link.addEventListener("click", onFollowLink);
     },
   };
 })();
